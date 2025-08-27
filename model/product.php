6e3244b80fa76bf0e27f5406ab9223c79a9562fe
m.agasht <M.Agasht>
2025-08-27 16:38:48 +0330

    تغییرات اولیه facade

--- model/product.php
+++ model/product.php
@@ -4,12 +4,11 @@ class product extends model{
     protected static $OBJ = null;
     protected static $table = ' product ';
     protected static $related = ['product_category' , 'category_id'];
-    protected static $subQuery = '';
     protected static $requestQuery = '';
 
-    public static function category(){
+    protected function category(){
         
-        return factory::factory(static::class) -> belongsTo( ' INNER ' , ' category ');
+        return $this -> belongsTo([ ' INNER ' , ' category ']);
 
         // return (static::makeOBJ()) -> belongsTo( ' INNER ' , ' category ');
     }
